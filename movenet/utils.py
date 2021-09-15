fa48401b29fb59f641439d6d397369d29752640e
lee-man <minli.amoy@gmail.com>
2021-09-15 20:14:13 +0800

    update,

--- movenet/utils.py
+++ movenet/utils.py
@@ -15,8 +15,13 @@ def valid_resolution(width, height, output_stride=16):
     return target_width, target_height
 
 
-def _process_input(source_img, size=192):
-    input_img = cv2.resize(source_img, (size, size),
+def _process_input(source_img, size=192, crop_region=None):
+    if crop_region != None:
+        input_img = source_img[crop_region['y_min']:crop_region['y_max'], crop_region['x_min']:crop_region['y_max']]
+        input_img = cv2.resize(input_img, (size, size),
+                           interpolation=cv2.INTER_LINEAR)
+    else:
+        input_img = cv2.resize(source_img, (size, size),
                            interpolation=cv2.INTER_LINEAR)
     input_img = cv2.cvtColor(input_img, cv2.COLOR_BGR2RGB).astype(np.float32)
     input_img = input_img.transpose((2, 0, 1)).reshape(1, 3, size, size)
@@ -24,11 +29,11 @@ def _process_input(source_img, size=192):
     return input_img, source_img
 
 
-def read_cap(cap, size=192):
+def read_cap(cap, size=192, crop_region=None):
     res, img = cap.read()
     if not res:
         raise IOError("webcam failure")
-    return _process_input(img, size)
+    return _process_input(img, size, crop_region)
 
 
 def read_imgfile(path, size=192):
@@ -56,7 +61,8 @@ def get_adjacent_keypoints(keypoint_scores, keypoint_coords, min_confidence=0.1)
         if keypoint_scores[left] < min_confidence or keypoint_scores[right] < min_confidence:
             continue
         results.append(
-            np.array([keypoint_coords[left][::-1], keypoint_coords[right][::-1]]).astype(np.int32),
+            np.array([keypoint_coords[left][::-1],
+                     keypoint_coords[right][::-1]]).astype(np.int32),
         )
     return results
 
@@ -102,3 +108,184 @@ def draw_skel_and_kp(
             flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
     out_img = cv2.polylines(out_img, adjacent_keypoints, isClosed=False, color=(255, 255, 0))
     return out_img
+
+
+
+'''
+Intelligent cropping algorithm borrowed from Movenet doc:
+https://www.tensorflow.org/hub/tutorials/movenet
+'''
+# Confidence score to determine whether a keypoint prediction is reliable.
+MIN_CROP_KEYPOINT_SCORE = 0.2
+
+# Dictionary that maps from joint names to keypoint indices.
+KEYPOINT_DICT = {
+    'nose': 0,
+    'left_eye': 1,
+    'right_eye': 2,
+    'left_ear': 3,
+    'right_ear': 4,
+    'left_shoulder': 5,
+    'right_shoulder': 6,
+    'left_elbow': 7,
+    'right_elbow': 8,
+    'left_wrist': 9,
+    'right_wrist': 10,
+    'left_hip': 11,
+    'right_hip': 12,
+    'left_knee': 13,
+    'right_knee': 14,
+    'left_ankle': 15,
+    'right_ankle': 16
+}
+
+
+def init_crop_region(image_height, image_width):
+  """Defines the default crop region.
+
+  The function provides the initial crop region (pads the full image from both
+  sides to make it a square image) when the algorithm cannot reliably determine
+  the crop region from the previous frame.
+  """
+#   if image_width > image_height:
+#     box_height = image_width / image_height
+#     box_width = 1.0
+#     y_min = (image_height / 2 - image_width / 2) / image_height
+#     x_min = 0.0
+#   else:
+#     box_height = 1.0
+#     box_width = image_height / image_width
+#     y_min = 0.0
+#     x_min = (image_width / 2 - image_height / 2) / image_width
+
+#   return {
+#     'y_min': y_min,
+#     'x_min': x_min,
+#     'y_max': y_min + box_height,
+#     'x_max': x_min + box_width,
+#     'height': box_height,
+#     'width': box_width
+#   }
+  return {
+    'y_min': 0,
+    'x_min': 0,
+    'y_max': image_height,
+    'x_max': image_width,
+    'height': 1.0, # image_width / image_height,
+    'width': 1.0
+  }
+
+def torso_visible(keypoints):
+  """Checks whether there are enough torso keypoints.
+
+  This function checks whether the model is confident at predicting one of the
+  shoulders/hips which is required to determine a good crop region.
+  """
+  return ((keypoints[KEYPOINT_DICT['left_hip'], 2] >
+           MIN_CROP_KEYPOINT_SCORE or
+          keypoints[KEYPOINT_DICT['right_hip'], 2] >
+           MIN_CROP_KEYPOINT_SCORE) and
+          (keypoints[KEYPOINT_DICT['left_shoulder'], 2] >
+           MIN_CROP_KEYPOINT_SCORE or
+          keypoints[KEYPOINT_DICT['right_shoulder'], 2] >
+           MIN_CROP_KEYPOINT_SCORE))
+
+def determine_torso_and_body_range(
+    keypoints, target_keypoints, center_y, center_x):
+  """Calculates the maximum distance from each keypoints to the center location.
+
+  The function returns the maximum distances from the two sets of keypoints:
+  full 17 keypoints and 4 torso keypoints. The returned information will be
+  used to determine the crop size. See determineCropRegion for more detail.
+  """
+  torso_joints = ['left_shoulder', 'right_shoulder', 'left_hip', 'right_hip']
+  max_torso_yrange = 0.0
+  max_torso_xrange = 0.0
+  for joint in torso_joints:
+    dist_y = abs(center_y - target_keypoints[joint][0])
+    dist_x = abs(center_x - target_keypoints[joint][1])
+    if dist_y > max_torso_yrange:
+      max_torso_yrange = dist_y
+    if dist_x > max_torso_xrange:
+      max_torso_xrange = dist_x
+
+  max_body_yrange = 0.0
+  max_body_xrange = 0.0
+  for joint in KEYPOINT_DICT.keys():
+    if keypoints[KEYPOINT_DICT[joint], 2] < MIN_CROP_KEYPOINT_SCORE:
+      continue
+    dist_y = abs(center_y - target_keypoints[joint][0]);
+    dist_x = abs(center_x - target_keypoints[joint][1]);
+    if dist_y > max_body_yrange:
+      max_body_yrange = dist_y
+
+    if dist_x > max_body_xrange:
+      max_body_xrange = dist_x
+
+  return [max_torso_yrange, max_torso_xrange, max_body_yrange, max_body_xrange]
+
+def determine_crop_region(
+      keypoints, image_height,
+      image_width):
+  """Determines the region to crop the image for the model to run inference on.
+
+  The algorithm uses the detected joints from the previous frame to estimate
+  the square region that encloses the full body of the target person and
+  centers at the midpoint of two hip joints. The crop size is determined by
+  the distances between each joints and the center point.
+  When the model is not confident with the four torso joint predictions, the
+  function returns a default crop which is the full image padded to square.
+  """
+  target_keypoints = {}
+  for joint in KEYPOINT_DICT.keys():
+    target_keypoints[joint] = [
+      keypoints[KEYPOINT_DICT[joint], 0] * image_height,
+      keypoints[KEYPOINT_DICT[joint], 1] * image_width
+    ]
+
+  if torso_visible(keypoints):
+    center_y = (target_keypoints['left_hip'][0] +
+                target_keypoints['right_hip'][0]) / 2;
+    center_x = (target_keypoints['left_hip'][1] +
+                target_keypoints['right_hip'][1]) / 2;
+
+    (max_torso_yrange, max_torso_xrange,
+      max_body_yrange, max_body_xrange) = determine_torso_and_body_range(
+          keypoints, target_keypoints, center_y, center_x)
+
+    crop_length_half = np.amax(
+        [max_torso_xrange * 1.9, max_torso_yrange * 1.9,
+          max_body_yrange * 1.2, max_body_xrange * 1.2])
+
+    tmp = np.array(
+        [center_x, image_width - center_x, center_y, image_height - center_y])
+    crop_length_half = np.amin(
+        [crop_length_half, np.amax(tmp)]);
+
+    crop_corner = [center_y - crop_length_half, center_x - crop_length_half];
+
+    if crop_length_half > max(image_width, image_height) / 2:
+      return init_crop_region(image_height, image_width)
+    else:
+      crop_length = crop_length_half * 2;
+      return {
+        'y_min': crop_corner[0], # / image_height,
+        'x_min': crop_corner[1], # / image_width,
+        'y_max': (crop_corner[0] + crop_length), # / image_height,
+        'x_max': (crop_corner[1] + crop_length), # / image_width,
+        'height': (crop_corner[0] + crop_length) / image_height -
+            crop_corner[0] / image_height,
+        'width': (crop_corner[1] + crop_length) / image_width -
+            crop_corner[1] / image_width
+      }
+  else:
+    return init_crop_region(image_height, image_width)
+
+def crop_and_resize(image, crop_region, crop_size):
+  """Crops and resize the image to prepare for the model input."""
+  boxes = [[crop_region['y_min'], crop_region['x_min'],
+          crop_region['y_max'], crop_region['x_max']]]
+  output_image = tf.image.crop_and_resize(
+      image, box_indices=[0], boxes=boxes, crop_size=crop_size)
+  return output_image
+
